0fac4f81c29816fe61fe0ccc56dd6c76ca0a7b9a
sy-nt <20110715@student.hcmute.edu.vn>
2023-05-31 04:18:54 +0700

    fix cors origin

--- index.js
+++ index.js
@@ -45,7 +45,11 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+    cors({
+        origin: "http://3.227.150.6:3000",
+    })
+);
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */

5b379ba5eb06076c7e892958d871cbcdc48848a6
sy-nt <20110715@student.hcmute.edu.vn>
2023-06-03 20:22:59 +0700

    comment

--- src/api/postApi.js
+++ src/api/postApi.js
@@ -6,7 +6,7 @@ const postApi = {
     getAll: () => axiosClient.get("posts"),
     getSearchPost: (params) => axiosClient.get(`posts/${params}/search`),
     getPostByUserId: (id) => axiosClient.get(`posts/${id}/posts`),
-    commentPost: ({ id, params }) => axiosClient(`comment/${id}`, params),
+    commentPost: ({ id, params }) => axiosClient.post(`comments/${id}`, params),
     likePost: ({ id, params }) => axiosClient.patch(`posts/${id}/like`, params),
     uploadPost: (params) =>
         axios.post(process.env.REACT_APP_API_URL + "/posts", params, {
